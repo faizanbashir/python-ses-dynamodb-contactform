7c053ee73ab3d75ccc2e1443f26f7b0ff2cf0e4c
ﻓﻴﻀﺎﻥ ﺍﻟﺒﺸﻴﺮ <faizan.bashir@softwaysolutions.com>
2018-06-12 23:45:56 +0530

    Saving Contact Form data to DynamoDB

--- handler.py
+++ handler.py
@@ -1,6 +1,9 @@
 import boto3
 from botocore.exceptions import ClientError
+import json
 import os
+import time
+import uuid
 
 client = boto3.client('ses')
 sender = 'faizan.ibn.bashir@gmail.com'
@@ -8,44 +11,62 @@ subject = 'Serverless Mail'
 configset = 'ConfigSet'
 charset = 'UTF-8'
 
+dynamodb = boto3.resource('dynamodb')
+
 def sendMail(event, context):
-    # Print event
     print(event)
 
     try:
-        firstname = event['body']['firstname']
-        lastname = event['body']['lastname']
-        recipient = event['body']['email']
-        message = event['body']['message']
-        content = 'Message from ' + firstname + ' ' + lastname + ', sez: ' + message
-
-        response = client.send_email(
-            Source=sender,
-            Destination={
-                'ToAddresses': [
-                    recipient,
-                ],
-            },
-            Message={
-                'Subject': {
-                    'Charset': charset,
-                    'Data': subject
-                },
-                'Body': {
-                    'Html': {
-                        'Charset': charset,
-                        'Data': content
-                    },
-                    'Text': {
-                        'Charset': charset,
-                        'Data': content
-                    }
-                }
-            }
-        )
+        data = event['body']
+        content = 'Message from ' + data['firstname'] + ' ' + data['lastname'] + ', sez: ' + data['message']
+        saveToDynamoDB(data)
+        response = sendMailToUser(data, content)
     except ClientError as e:
         print(e.response['Error']['Message'])
     else:
         print("Email sent! Message Id:"),
         print(response['MessageId'])
     return "Email sent!"
+
+def saveToDynamoDB(data):
+    timestamp = int(time.time() * 1000)
+    # Insert details into DynamoDB Table
+    table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
+    item = {
+        'id': str(uuid.uuid1()),
+        'firstname': data['firstname'],
+        'lastname': data['lastname'],
+        'email': data['email'],
+        'message': data['message'],
+        'createdAt': timestamp,
+        'updatedAt': timestamp
+    }
+    table.put_item(Item=item)
+    return
+
+def sendMailToUser(data, content):
+    # Send Email using SES
+    return client.send_email(
+        Source=sender,
+        Destination={
+            'ToAddresses': [
+                data['email'],
+            ],
+        },
+        Message={
+            'Subject': {
+                'Charset': charset,
+                'Data': subject
+            },
+            'Body': {
+                'Html': {
+                    'Charset': charset,
+                    'Data': content
+                },
+                'Text': {
+                    'Charset': charset,
+                    'Data': content
+                }
+            }
+        }
+    )
