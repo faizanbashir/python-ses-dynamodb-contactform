ba380716e498591e88a5ef74a2a53dadf87c0d34
ﻓﻴﻀﺎﻥ ﺍﻟﺒﺸﻴﺮ <faizan.bashir@softwaysolutions.com>
2018-06-13 01:07:17 +0530

    Added JSON reponse python bug workaround

--- handler.py
+++ handler.py
@@ -83,3 +83,10 @@ def sendMailToUser(data, content):
             }
         }
     )
+
+# This is a workaround for: http://bugs.python.org/issue16535
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, decimal.Decimal):
+            return int(obj)
+        return super(DecimalEncoder, self).default(obj)
