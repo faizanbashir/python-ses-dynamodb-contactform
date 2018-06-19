4647f0e186f885a3e3964817b88c9536d7f41919
faizanbashir <faizan-bashir@live.com>
2018-06-19 23:53:19 +0530

    Updated DynamoDB permissions

---
+++ test.py
@@ -0,0 +1,14 @@
+import unittest
+import json
+from handler import sendMail, list
+
+class TestHandler(unittest.TestCase):
+    """" Tests handler methods """
+    def test_sendMail(self):
+        return "TBD"
+    
+    def test_list(self):
+        return "TBD"
+
+if __name__ == '__main__':
+    unittest.main()
